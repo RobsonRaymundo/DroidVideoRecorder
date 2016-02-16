2cfbe91b315f3ebec0e9239cff68c7a479b40dc9
Robson Raymundo <robson.raymundo@gmail.com>
2016-02-16 18:22:06 -0200

    correcao na notificacao do lollipop e melhoria no codigo
    correcao para nao aparecer a notificacao no lollipop atraves do comando
    por texto e melhoria no codigo

--- app/src/main/java/com/droid/videoRecorder/DroidNotification.java
+++ app/src/main/java/com/droid/videoRecorder/DroidNotification.java
@@ -2,48 +2,41 @@ package com.droid.videoRecorder;
 
 import android.app.Notification;
 import android.content.Intent;
-import android.os.Build;
 import android.os.Bundle;
-import android.service.notification.NotificationListenerService;
 import android.service.notification.StatusBarNotification;
-import java.lang.reflect.Field;
-import java.util.ArrayList;
 
 /**
  * Created by Robson on 03/02/2016.
  */
 
-public class DroidNotification extends NotificationListenerService {
+public class DroidNotification extends DroidBaseNotification {
 
-    @Override
-    public void onCreate() {
-        super.onCreate();
-    }
+    private boolean sentBroadcast = false;
 
     @Override
     public void onNotificationPosted(StatusBarNotification sbn) {
-
-        // super.onNotificationPosted(sbn);
-        String msgNotification;
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT) {
-            msgNotification = getNotificationKitKat(sbn);
-        } else {
-            msgNotification = getNotification(sbn.getNotification());
-        }
+        String msgNotification = getNotificationKitKat(sbn);
 
         if (!msgNotification.isEmpty()) {
-            Intent mIntent = new Intent();
-            mIntent.setAction(DroidConstants.CHAVERECEIVER);
-            mIntent.addCategory(Intent.CATEGORY_DEFAULT);
-            mIntent.putExtra(DroidConstants.CHAVERECEIVER, msgNotification);
-            sendBroadcast(mIntent);
-            this.cancelAllNotifications();
+            cancelAllNotifications();
+            if (!sentBroadcast) {
+                SendBroadCast(msgNotification);
+            }
         }
     }
 
     @Override
     public void onNotificationRemoved(StatusBarNotification sbn) {
-        super.onNotificationRemoved(sbn);
+        sentBroadcast = false;
+    }
+
+    private void SendBroadCast(String msgNotification) {
+        Intent mIntent = new Intent();
+        mIntent.setAction(DroidConstants.CHAVERECEIVER);
+        mIntent.addCategory(Intent.CATEGORY_DEFAULT);
+        mIntent.putExtra(DroidConstants.CHAVERECEIVER, msgNotification);
+        sendBroadcast(mIntent);
+        sentBroadcast = true;
     }
 
     private String getNotificationKitKat(StatusBarNotification mStatusBarNotification) {
@@ -52,11 +45,19 @@ public class DroidNotification extends NotificationListenerService {
         CharSequence tit = extras.getCharSequence(Notification.EXTRA_TITLE); // Title
         CharSequence desc = extras.getCharSequence(Notification.EXTRA_TEXT); // / Description
         String msg = "";
-        if (desc == null) {
+
+        try {
             Bundle bigExtras = mStatusBarNotification.getNotification().extras;
             CharSequence[] descArray = bigExtras.getCharSequenceArray(Notification.EXTRA_TEXT_LINES);
             msg = descArray[descArray.length - 1].toString();
-        } else msg = desc.toString();
+
+        } catch (Exception ex) {
+
+        }
+
+        if (msg.isEmpty()) {
+            msg = desc.toString();
+        }
         String msgCMD = msg.substring(0, DroidConstants.COMANDOINICIADOPOR.length());
 
         if (DroidConstants.COMANDOINICIADOPOR.equalsIgnoreCase(msgCMD)) {
@@ -64,49 +65,4 @@ public class DroidNotification extends NotificationListenerService {
         } else return "";
     }
 
-    public static Object getObjectProperty(Object object, String propertyName) {
-        try {
-            Field f = object.getClass().getDeclaredField(propertyName);
-            f.setAccessible(true);
-            return f.get(object);
-        } catch (NoSuchFieldException e) {
-            e.printStackTrace();
-        } catch (IllegalAccessException e) {
-            e.printStackTrace();
-        }
-        return null;
-    }
-
-    private String getNotification(Notification notif) {
-        String sValue;
-        String sMethodName;
-        try {
-
-            ArrayList<Object> actions = (ArrayList<Object>) getObjectProperty(getObjectProperty(notif, "bigContentView"), "mActions");
-            Field fMethodName;
-            Field fValue;
-            for (int i = actions.size() - 1; i >= actions.size() - 2; i--) {
-                fMethodName = actions.get(i).getClass().getDeclaredField("methodName");
-                fMethodName.setAccessible(true);
-                sMethodName = fMethodName.get(actions.get(i)).toString();
-                try {
-                    if (sMethodName.equals("setText")) {
-                        fValue = actions.get(i).getClass().getDeclaredField("value");
-                        fValue.setAccessible(true);
-                        sValue = fValue.get(actions.get(i)).toString().substring(0, DroidConstants.COMANDOINICIADOPOR.length());
-                        String valueCMD = sValue.substring(0, DroidConstants.COMANDOINICIADOPOR.length());
-                        if (DroidConstants.COMANDOINICIADOPOR.equalsIgnoreCase(valueCMD)) {
-                            return sValue;
-                        } else return "";
-                    }
-                } catch (Exception ex) {
-                    //sValue = ex.getMessage();
-                }
-            }
-        } catch (Exception ex) {
-            String mEx = ex.getMessage();
-        }
-        return "";
-
-    }
 }
